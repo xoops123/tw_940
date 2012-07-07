f85a955404e406439e05ad3b290dfe8848da3b0c
marine <marine@mistgreen.net>
2012-07-07 23:41:17 +0900

    new repo

---
+++ html/themes/tw_940/manifesto.ini.php
@@ -0,0 +1,21 @@
+<?php
+/**
+
+[Manifesto]
+Name="tw_940"
+Depends=Legacy_RenderSystem,legacy
+Url="http://xoops123.com/"
+Version="0.01"
+
+[Theme]
+RenderSystem=Legacy_RenderSystem
+Format="XCL html5 Style"
+Author=Twitter Bootstrap - marine/mistgreen
+ScreenShot="screenshot.png"
+Description="Responsive Web Design"
+W3C=YES
+
+Licence="MIT"
+
+*/
+?>
